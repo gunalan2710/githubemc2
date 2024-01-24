5185ac2dfc6bdda7e5cb3ec6e8696df2e176a768
gunalan2710 <gunal1918@gmail.com>
2024-01-23 19:25:53 -0800

    index.java file is created

---
+++ index.java
@@ -0,0 +1,16 @@
+package githubemc2;
+
+/**
+ * index
+ */
+public class index {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+
+
+        
+    }
+}
